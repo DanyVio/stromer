e7e59349658dab60818a77acbe1315ee1ed0243e
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-11-13 15:57:00 +0100

    fine adjustments on the language selector

--- cypress/e2e/tests/Dave generated/st1s/configuratorFrench.cy.js
+++ cypress/e2e/tests/Dave generated/st1s/configuratorFrench.cy.js
@@ -22,7 +22,7 @@ describe('Bike Configurator', function () {
 
   //Select an E-bike
 
-  it('Your Stromer -> ST1', function () {
+  it('Your Stromer -> ST1s', function () {
     cy.visit(`${config.baseUrl}`+'/fr/configurator?bike=st1s');
 
     cy.get('#edit-content--2 > .language-countries-options-container > [data-country="fr"][data-language="fr"]')
