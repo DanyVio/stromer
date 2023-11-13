5755a5cd701ad8c077598915a160974a8cdb7370
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-11-13 14:38:45 +0100

    updating language field in all 4 languages

--- cypress/e2e/tests/Dave generated/st1s/configuratorDutch.cy.js
+++ cypress/e2e/tests/Dave generated/st1s/configuratorDutch.cy.js
@@ -3,12 +3,9 @@ describe('Bike Configurator', function () {
 
   beforeEach('before each test', function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.Popup__PopupCloseInsideInner-sc-17yhal5-5 button')
+    cy.get('#popup-buttons')
       .should('be.visible')
       .click({ multiple: true });
-    cy.get('.agree-button')
-      .should('be.visible')
-      .click({ force: true });
   });
 
   //Select a language and country
@@ -18,32 +15,19 @@ describe('Bike Configurator', function () {
       .should('be.visible')
       .click();
 
-    cy.get('#aos-locale-multi-switcher-form--2 .custom-js-dropdown.custom-js-dropdown-language')
-      .should('be.visible')
-      .click();
-
-    cy.get('#aos-locale-multi-switcher-form--2 .custom-js-dropdown-option.custom-js-dropdown-language-option').contains('Nederlands')
-      .should('be.visible')
-      .click();
-
-    cy.get('#aos-locale-multi-switcher-form--2 [cdata-selector-target="aos-multi-switcher-country-selector"]')
-      .should('be.visible')
-      .click();
-
-    cy.get('#aos-locale-multi-switcher-form--2 .custom-js-dropdown-option.custom-js-dropdown-country-options').contains('Netherlands')
-      .should('be.visible')
-      .click();
-
-    cy.get('#edit-submit--2')
+    cy.get('#edit-content--2 > .language-countries-options-container > [data-country="nl"][data-language="nl"]')
       .should('be.visible')
       .click();
   });
 
   //Select an E-bike
 
-  it('Your Stromer -> ST1', function () {
+  it('Your Stromer -> ST1s', function () {
     cy.visit(`${config.baseUrl}`+'/nl/configurator?bike=st1s');
-    
+
+    cy.get('#edit-content--2 > .language-countries-options-container > [data-country="nl"][data-language="nl"]')
+      .should('be.visible')
+      .click();
     cy.get('[for="Rahmengrösse_l0"]')
       .should('be.visible')
       .click();
