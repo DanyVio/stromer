4e53e82819fadb52dd9f25228a9e5cfe12affe3e
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-11-27 13:55:55 +0100

    pageLoadTimeout changed

--- cypress.config.js
+++ cypress.config.js
@@ -11,7 +11,8 @@ module.exports = defineConfig({
     viewportHeight: 1280,
     specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
   },
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 120000,
+  pageLoadTimeout: 120000,
   execTimeout: 120000,
   taskTimeout: 120000,
   numTestsKeptInMemory: 0
